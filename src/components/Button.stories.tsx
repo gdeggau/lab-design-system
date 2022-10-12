14c3465689d5beb65311ff9f94bdc31769823fae
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-10-12 12:29:46 -0300

    add Button component

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Button, ButtonProps } from "./Button";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    children: "Create button",
+  },
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {};
