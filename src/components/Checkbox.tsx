cdc7cdd8c8cef8dc2201a10032db79ad4817dcbb
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-10-14 10:19:40 -0300

    add Login page

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,11 +2,14 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { ReactNode } from "react";
 
-export type CheckboxProps = {};
+export type CheckboxProps = CheckboxPrimitive.CheckboxProps & {};
 
-export const Checkbox = ({}: CheckboxProps) => {
+export const Checkbox = (props: CheckboxProps) => {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
+    >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
